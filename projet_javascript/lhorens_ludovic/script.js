e6ef99b9e5aa9aa0f4c586172b004878f8c66af3
Llhorens <85550607+Llhorens@users.noreply.github.com>
2021-06-28 15:21:36 +0200

    Add files via upload

---
+++ projet_javascript/lhorens_ludovic/script.js
@@ -0,0 +1,8 @@
+const card = document.querySelectorAll('.card__inner');
+
+card.forEach((card) => {
+     card.addEventListener("click", () => {
+          card.classList.toggle("is-flipped");
+     });
+});
+
