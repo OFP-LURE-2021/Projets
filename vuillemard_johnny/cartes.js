b581da82441fbf992ee07ba184e34f13efdc29c1
vuillemard <85550833+vuillemard@users.noreply.github.com>
2021-06-29 09:53:21 +0200

    Delete vuillemard_johnny directory

--- vuillemard_johnny/cartes.js
+++
@@ -1,9 +0,0 @@
-const card = document.querySelectorAll(".card");
-
-console.log(card);
-
-card.forEach((card) => {
-  card.addEventListener("click", () => {
-    card.classList.toggle("flipCard");
-  });
-});
